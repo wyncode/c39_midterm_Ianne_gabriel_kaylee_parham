2bb0dc9730de808f894edab622a4b666dc7353be
kaylee2319 <kayleeharding23@gmail.com>
2020-10-26 11:32:45 -0400

    Updated HomePage with the Pokemon cards on the page, still need a little work but almost done

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,9 +1,44 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 
 
 function HomePage() {
+    const [venaData, setVenaData] = useState({});
+    const [duckData, setDuckData] = useState({});
+    const [genData, setGenData] = useState({});
+    
+    useEffect(() => {
+        const fetchData = async () => {
+          let response = await axios.get(
+            `https://api.pokemontcg.io/v1/cards/ex14-28`
+          );
+          setVenaData(response.data);
+        };
+         fetchData();
+      }, []);
 
+
+      useEffect(() => {
+        const fetchData1 = async () => {
+          let duck = await axios.get(
+              `https://api.pokemontcg.io/v1/cards/base5-65`
+            );
+          setDuckData(duck.data);
+        };
+         fetchData1();
+      }, []);
+
+
+    useEffect(() => {
+      const fetchData2 = async () => {
+        let gengar = await axios.get(
+            `https://api.pokemontcg.io/v1/cards/sm9-53`
+          );
+        setGenData(gengar.data);
+      };
+       fetchData2();
+    }, []);
   return (
           <body className="HomePageBody">
               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
@@ -13,9 +48,8 @@ function HomePage() {
             </div>
             <div className='suggestions'>
                 <ul className='suggestedPowers'>
-                    <li className='powerType'><button>fire</button></li>
-                    <li className='powerType'><button>fire</button></li>
-                    <li className='powerType'><button>fire</button></li>
+                    <p className='powerType' ><button className ='psyPokemon'><img src={(duckData.card && duckData.card.imageUrl)}></img></button></p> 
+                    <p className='powerType' ><button className ='grassPokemon'><img src={(venaData.card && venaData.card.imageUrl)}></img></button></p> 
                 </ul>
             </div>
             <div class="icon-bar">
