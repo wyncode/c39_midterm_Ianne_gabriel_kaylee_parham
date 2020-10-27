0565bd149d3161bf864af959594faf01b5d9cbe8
itchoib <itchoib@gmail.com>
2020-10-27 10:49:23 -0400

    added props for link

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Card = (props) => {
     return (
         <div className="container">
-            <img alt="blastoise" src={props.image}/>
+            <a href={`/card/${key}`}>
+                <img alt="blastoise" src={props.image}/>
+            </a>
             <div className="middle">
-    <h1 className="text"> {props.name}</h1>
+                 <h1 className="text"> {props.name}</h1>
             </div>
         </div>
     )
